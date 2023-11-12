ce0736d093e9754958bed34668b20cf118743a5b
Arkin Maduri <arkin.maduri@gmail.com>
2023-11-12 11:50:18 -0800

    Fixed the bug for num equals to 1

--- math_challenges/TriangularNumber.py
+++ math_challenges/TriangularNumber.py
@@ -1,6 +1,6 @@
 
 def is_num_triagular(num):
-    if num <= 1:
+    if num < 1:
         return False
     if num == 1:
         return True
@@ -11,7 +11,7 @@ def is_num_triagular(num):
     return num == 0
 
 if __name__ == "__main__":
-    for a in range(1,100):
+    for a in range(1,10000):
         if is_num_triagular(a):
             print ("I am a triangular number " + str(a))
         else:
