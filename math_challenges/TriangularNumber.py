e5906f8052d2daec8effc03bdd84c74daa75fcda
Arkin Maduri <arkin.maduri@gmail.com>
2023-11-12 11:45:43 -0800

    Triangular number challenge

---
+++ math_challenges/TriangularNumber.py
@@ -0,0 +1,18 @@
+
+def is_num_triagular(num):
+    if num <= 1:
+        return False
+    if num == 1:
+        return True
+    start = 1
+    while num > 0:
+        num = num - start
+        start = start + 1
+    return num == 0
+
+if __name__ == "__main__":
+    for a in range(1,100):
+        if is_num_triagular(a):
+            print ("I am a triangular number " + str(a))
+        else:
+            print ("I am not a triangular number " + str(a))
